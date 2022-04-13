6791b75e1eadd4fb3b55f0f804e06bd529764db5
User <abdurasilov99@gmail.com>
2022-04-13 18:34:20 +0600

    first commit

--- console.sql
+++ console.sql
@@ -12,24 +12,24 @@ VALUES ('iPhone X', 'Apple', 2, 71000,true),
        ('Galaxy S9', 'Samsung', 6, 56000,false),
        ('Galaxy S8 Plus', 'Samsung', 2, 46000,true),
        ('Desire 12', 'HTC', 3, 26000,true);
---1
+--1--
 SELECT productName,manufacturer,price FROM products WHERE manufacturer NOT IN ('HTC');
---2
+--2--
 SELECT * FROM products WHERE manufacturer = 'Apple' AND price < 75000;
---3
+--3--
 SELECT * FROM products WHERE  price>=46000;
---4
-SELECT  productName,manufacturer,productCount,price  FROM products LIMIT 2;
---5
-SELECT  MIN(price) FROM products manufacturer;
---6
-SELECT  productName,manufacturer,price,isDiscounted FROM products WHERE isDiscounted=true;
---7
-SELECT  productName,manufacturer,price,isDiscounted FROM products WHERE isDiscounted=false;
---8
-SELECT productName, manufacturer,price FROM products;
---9
+--4--
+SELECT  *  FROM products  ORDER BY price DESC LIMIT 2;
+--5--
+SELECT * FROM products WHERE price=(SELECT MIN(price) FROM products);
+--6--
+SELECT  * FROM products WHERE isDiscounted=true;
+--7--
+SELECT  * FROM products WHERE isDiscounted=false;
+--8--
+SELECT * FROM products ORDER BY  price DESC;
+--9--
 SELECT SUM(price) FROM products;
---10
-SELECT productName,manufacturer,productCount,price FROM products WHERE productCount>=2;
+--10--
+SELECT * FROM products WHERE productCount>=2;
 
